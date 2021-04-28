b81db78e83294423031cb5de96ef6230d1a79daf
Nalaka Manathunga <mxnalaka@gmail.com>
2021-04-28 15:33:55 +0200

    fix: updates sidebar styling

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className="w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16 rounded-2xl z-40">
+    <header className="w-full shadow-lg bg-white dark:bg-gray-700 items-center h-16  z-40">
       <div className="relative z-20 flex flex-col justify-center h-full px-3 mx-auto flex-center">
         <div className="relative items-center pl-1 flex w-full lg:max-w-68 sm:pr-2 sm:ml-0">
           <div className="container relative left-0 z-50 flex w-3/4 h-auto h-full">
